6eec6f4c2992b6c94cddaea0dfe958cc95c8a036
Zach Brownson <zbrownson@hotmail.com>
2018-06-29 20:10:05 -0700

    initial checkin

---
+++ app/app.js
@@ -0,0 +1,68 @@
+const local_app = function () {}
+  
+const express = require('express')
+const https = require('https')
+const nodemailer = require('nodemailer')
+const xml2js = require('xml2js')
+const _ = require('underscore')
+//const config = require('./config')
+const bodyParser = require("body-parser");
+const csvtojson = require("csv-to-json");
+const port = 3000
+
+// * ———————————————————————————————————————————————————————— * //
+// * 	init
+// *
+// *	gets called upon starting enduro.js production server
+// *	@param {express app} app - express app
+// *	@return {nothing}
+// * ———————————————————————————————————————————————————————— * //
+local_app.prototype.init = function (app) {
+	
+	// server js libaries
+	app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+	app.use('/gridstack', express.static(__dirname + '/node_modules/gridstack/dist/'));
+	app.use('/underscore', express.static(__dirname + '/node_modules/underscore/'));
+
+	//Here we are configuring express to use body-parser as middle-ware.
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(bodyParser.json());
+	
+	// express app available here
+	// don't forget these routes will be available on production server server (defaults to localhost:5000)
+	app.get('/api/get_random_number', function (req, res) {
+		res.send(Math.random().toString())
+	})
+	
+	app.get('/draftboard', (request, response) => {  
+
+		var format = request.query.format || 'standard';
+		var teams = request.query.teams;
+		
+		if (teams === '8 team') {
+			teams = 8;
+		}
+		else if (teams === '10 team') {
+			teams = 10;
+		}
+		else if (teams === '12 team') {
+			teams = 12;
+		}
+		else if (teams === '14 team') {
+			teams = 14;
+		}
+		else {
+			teams = 12;
+		}
+		
+		var obj = {
+			filename: format === "PPR" ? 'assets/data/ppr_rank.csv' : 'assets/data/standard_rank.csv'
+		};
+		csvtojson.parse(obj, function(err, json) {
+			response.send(JSON.stringify(json));
+		});
+	})
+
+}
+
+module.exports = new local_app()
---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'zachb',
+			tags: [],
+			salt: 'be67c63e11c73256a13043d76383fb9e',
+			hash: 'ff62a43268d1a88f2d6ac21a6fd0ab51d07c549ac6de7c924a4659b29252af99',
+			user_created_timestamp: 1530234874351
+		}
+	]
+})
